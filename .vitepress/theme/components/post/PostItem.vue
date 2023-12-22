6418cac8b5bb31980f430716b57a109ce432f88a
liuyichen <2643150172@qq.com>
2023-12-22 10:28:32 +0800

    add load img

--- .vitepress/theme/components/post/PostItem.vue
+++ .vitepress/theme/components/post/PostItem.vue
@@ -1,6 +1,7 @@
 <script setup>
-import  {computed} from "vue";
+import {computed, onBeforeUnmount, onMounted, ref} from "vue";
 import {withBase} from 'vitepress'
+import LazyLoad from "vanilla-lazyload";
 
 const props = defineProps({
   title: {
@@ -20,8 +21,37 @@ const props = defineProps({
   }
 })
 
+const imgUrls = [
+    'https://picsum.photos/id/20/367/267',
+    'https://picsum.photos/id/24/367/267',
+    'https://picsum.photos/id/25/367/267',
+    'https://picsum.photos/id/0/367/267',
+    'https://picsum.photos/id/42/367/267',
+    'https://picsum.photos/id/48/367/267',
+    'https://picsum.photos/id/36/367/267',
+    'https://picsum.photos/id/39/367/267'
+]
+
 const showImgUrl = computed(() => {
-  return props.image || 'https://picsum.photos/id/20/367/267'
+  const randomIndex = Math.floor(Math.random() * imgUrls.length)
+  return props.image || imgUrls[randomIndex]
+})
+
+const lazyLoadInstance = new LazyLoad({
+  // css属性选择器
+  elements_selector: 'img',
+  // 滚动多少加载
+  threshold: 0,
+  // 加载的元素URL的属性
+  data_src: 'lazy-src'
+})
+
+onMounted(() => {
+  lazyLoadInstance.update();
+})
+
+onBeforeUnmount(() => {
+  lazyLoadInstance.destroy();
 })
 
 </script>
@@ -30,7 +60,7 @@ const showImgUrl = computed(() => {
   <div class="relative flex gap-5 border-b p-3.5 duration-300  border-gray-200 dark:border-gray-700">
     <div class="post-left relative flex-1">
       <a :href="url" class="absolute top-0 left-0 w-full h-full overflow-hidden rounded">
-        <img class="object-cover w-full h-full" :src="showImgUrl" loading="lazy" alt="">
+        <img src="/img/img-loading.svg" class="object-cover w-full h-full" :data-lazy-src="showImgUrl" alt="" ref="imgRef">
       </a>
     </div>
     <div class="flex flex-[2.5_2.5_0%] flex-col justify-between w-0 flex-shrink-0">
--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,11 +1,12 @@
 // https://vitepress.dev/guide/custom-theme
 import Layout from './Layout.vue'
+import LazyLoad from "vanilla-lazyload";
 import './styles/style.css'
 
 /** @type {import('vitepress').Theme} */
 export default {
   Layout,
-  enhanceApp({ app, router, siteData }) {
+  enhanceApp({app, router, siteData}) {
     // ...
   }
 }
